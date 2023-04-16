0c5b43d5b99f07c49edcc1ecd47051b1f3f40956
annakopeikina <anna.v.kopeikina@gmail.com>
2023-04-16 17:12:48 +0400

    python23 non-working variation

--- python23/main.py
+++ python23/main.py
@@ -2,22 +2,24 @@
 # Напишите программу, которая подсчитает количество элементов массива,
 # больших предыдущего (элемента с предыдущим номером)
 # Input: [0, -1, 5, 2, 3] Output: 2 (-1 < 5, 2 < 3)
+
 import random
 print("задайте количество чисел в массиве; ")
 n = int(input())
 list = [random.randint(1, n) for _ in range(n)]
 print(list)
-i = 0
-count = 0
 
+for i in list:
+    compare = []
+    idx = list.index(i)
+    for j in range(len(list)):
+        if (idx == j):
+            continue
+        compare.append(i > list[j])
+print(compare)
 
-for i in range(n + 1):
-    a = list[i]
-    b = list[i+1]
-    if n < (i + 1):
-        break
-    for count in range(n):
-        if a < b:
-            count = count + 1
-            i += 1
+count = list[0]
+for m in list:
+    if (count < m):
+        count = m
 print(count)
