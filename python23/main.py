6212910968e70141d3b480b7b2e21fe9122cffc6
annakopeikina <anna.v.kopeikina@gmail.com>
2023-04-16 22:32:12 +0400

    python23 has been committed

--- python23/main.py
+++ python23/main.py
@@ -8,21 +8,10 @@ print("задайте количество чисел в массиве: ")
 n = int(input())
 list = [random.randint(1, n) for _ in range(n)]
 print(list)
-
-for i in list:  # transform boolean in to digit comparison
-    compare = []
-    idx = list.index(i)
-    for j in range(len(list)):
-        if (idx == j):
-            continue
-        else:
-            compare.append(i > list[j])
-print(compare)
-
-count = list[0]  # transform find smollest in comparison
-for m in list:
-    if (count == m):
-        continue
-    elif (count > m):
-        count = m
+count = 0
+for i in range(len(list) - 1):
+    if list[i + 1] > list[i]:
+        count += 1
+    if list[i] < list[i + 1]:
+        print(f"{list[i]} < {list[i + 1]}")
 print(count)
