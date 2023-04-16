c3384641f745563cd073724048048050ac02bd1e
annakopeikina <anna.v.kopeikina@gmail.com>
2023-04-16 19:26:31 +0400

    new branch" comparison" for modifying code is created

--- python23/main.py
+++ python23/main.py
@@ -4,22 +4,25 @@
 # Input: [0, -1, 5, 2, 3] Output: 2 (-1 < 5, 2 < 3)
 
 import random
-print("задайте количество чисел в массиве; ")
+print("задайте количество чисел в массиве: ")
 n = int(input())
 list = [random.randint(1, n) for _ in range(n)]
 print(list)
 
-for i in list:
+for i in list:  # transform boolean in to digit comparison
     compare = []
     idx = list.index(i)
     for j in range(len(list)):
         if (idx == j):
             continue
-        compare.append(i > list[j])
+        else:
+            compare.append(i > list[j])
 print(compare)
 
-count = list[0]
+count = list[0]  # transform find smollest in comparison
 for m in list:
-    if (count < m):
+    if (count == m):
+        continue
+    elif (count > m):
         count = m
 print(count)
