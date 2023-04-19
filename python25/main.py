1b07edcd39ac4b5fe3eb5e3524d222f74231a942
annakopeikina <anna.v.kopeikina@gmail.com>
2023-04-19 15:43:30 +0400

    perfect solution has been added for python25 and python26 for selfcheck

--- python25/main.py
+++ python25/main.py
@@ -21,3 +21,24 @@ for x in set_1:
     if x in set_2:
         result.add(x)
 print(*sorted(result))
+
+
+# mol = [int(x) for x in input().split()]
+# n = mol[0]
+# m = mol[1]
+# set_1 = set()
+# set_2 = set()
+# list_1 = list()
+# a = [int(x) for x in input().split()]
+# k = set(a)
+# for i in k:
+#    set_1.add(i)
+# b = [int(x) for x in input().split()]
+# k1 = set(b)
+# for i in k1:
+#    set_2.add(i)
+# lok = set_1 & set_2
+# kool = list(lok)
+# kool.sort()
+# for i in kool:
+#    print(i, end=' ')
--- python26/main.py
+++ python26/main.py
@@ -28,3 +28,16 @@ for i in range(n):
     if berries > max_berry_count:
         max_berry_count = berries
 print(f"Максимальный сбор: {max_berry_count}")
+
+
+# n = int(input())
+# arr = list()
+# for i in range(n):
+#    x = int(input)
+#   arr.append(x)
+
+# arr_count = list()
+# for i in range((len.arr)-1):
+#    arr_count.append(arr(i-1) + arr(i) + arr(i+1))
+# arr_count.append(arr[2]) + (arr[1]) +(arr[0])
+# print(max(arr_count))
